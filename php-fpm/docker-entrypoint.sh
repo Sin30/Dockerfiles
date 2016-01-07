302b52f9e7a7cf8681f9294da27844da16a7ac82
Jason Guo <sin30.net@gmail.com>
2016-01-07 13:53:03 +0800

    create entrypoint.sh for php-fpm

---
+++ php-fpm/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+export APP_ENV=${APP_ENV:-prod}
+
+if [ "$1" = 'php-fpm' ]; then
+	chown -R www-data .
+	cd /usr/local/etc/php && ln -sf php.ini-${APP_ENV} php.ini && cd -
+	exec php-fpm
+fi
+
+exec "$@"
