b030f253643f18584c6fa09ad6b21d36a1202198
Jason Guo <sin30.net@gmail.com>
2016-01-07 15:51:48 +0800

    allocate huge pages for php-fpm

--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -7,6 +7,8 @@ export APP_ENV=${APP_ENV:-prod}
 if [ "$1" = 'php-fpm' ]; then
 	chown -R www-data .
 	cd /usr/local/etc/php && ln -sf php.ini-${APP_ENV} php.ini && cd -
+	# allocate some huge pages for php-fpm
+	sysctl vm.nr_hugepages=128
 	exec php-fpm
 fi
 
