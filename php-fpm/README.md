578c13de56a7e97aac52e9ef2a6ea47bfe6df857
Jason Guo <sin30.net@gmail.com>
2016-03-24 10:52:58 +0800

    cancel privilege mode for php-fpm due to unsupported huge pages on alpine linux

--- php-fpm/README.md
+++ php-fpm/README.md
@@ -1,7 +1,5 @@
 # Docker image for PHP-FPM
 
-This image has to run under privilege mode in order to allocate Hugepages for php-fpm using `sysctl vm.nr_hugepages=128`
-
 ```shell
-docker run -d --privileged sin30/php-fpm
+docker run -d sin30/php-fpm
 ```
--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -7,9 +7,6 @@ export APP_ENV=${APP_ENV:-prod}
 if [ "$1" = 'php-fpm' ]; then
 	chown -R www-data .
 	cd /usr/local/etc/php && ln -sf php.ini-${APP_ENV} php.ini && cd -
-	# allocate some huge pages for php-fpm
-	# alpine linux seems to have no support for hugepages
-	# sysctl vm.nr_hugepages=128
 	exec php-fpm
 fi
 
