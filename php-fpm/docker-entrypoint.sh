e4652ac995d42a5eadaf3e3be82fdb962900a4d9
Jason Guo <sin30.net@gmail.com>
2016-03-24 10:00:27 +0800

    switch linux to alpine to reduce image size

--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eo pipefail
 
@@ -8,7 +8,8 @@ if [ "$1" = 'php-fpm' ]; then
 	chown -R www-data .
 	cd /usr/local/etc/php && ln -sf php.ini-${APP_ENV} php.ini && cd -
 	# allocate some huge pages for php-fpm
-	sysctl vm.nr_hugepages=128
+	# alpine linux seems to have no support for hugepages
+	# sysctl vm.nr_hugepages=128
 	exec php-fpm
 fi
 
--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eo pipefail
 
-if [ "$1" = 'redis-server' ]; then
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
 	echo 1 > /proc/sys/vm/overcommit_memory
 	echo 2048 > /proc/sys/net/core/somaxconn
 	echo never > /sys/kernel/mm/transparent_hugepage/enabled
-	exec gosu redis "$@"
+	exec gosu redis "$0" "$@"
 fi
 
 exec "$@"
