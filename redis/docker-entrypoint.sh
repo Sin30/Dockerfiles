e1d30cb947b2b37bbadf49bbf237978e21e2ad97
Jason Guo <sin30.net@gmail.com>
2016-04-14 22:05:23 +0800

    remove redis privileged mode to suit Docker for Mac

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -5,9 +5,6 @@ set -eo pipefail
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
-	echo 1 > /proc/sys/vm/overcommit_memory
-	echo 2048 > /proc/sys/net/core/somaxconn
-	echo never > /sys/kernel/mm/transparent_hugepage/enabled
 	exec gosu redis "$0" "$@"
 fi
 
