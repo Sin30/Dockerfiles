6d62071017fbb3ae9acb8602b8fdd7c9b6c741ca
Jason Guo <sin30.net@gmail.com>
2016-06-01 13:54:45 +0800

    do not change own

--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -5,7 +5,6 @@ set -eo pipefail
 export APP_ENV=${APP_ENV:-production}
 
 if [ "$1" = 'php-fpm' ]; then
-	chown -R www-data .
 	cd /usr/local/etc/php && ln -sf php.ini-${APP_ENV} php.ini && cd -
 	exec php-fpm
 fi
